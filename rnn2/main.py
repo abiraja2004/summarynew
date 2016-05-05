fd881f484855e5fb1f4c8d6b5ad88fd9d331ce81
HyNguyen <giahy2507@gmail.com>
2016-05-05 18:31:29 +0700

    parse tree

--- rnn2/main.py
+++ rnn2/main.py
@@ -1,5 +1,41 @@
 __author__ = 'HyNguyen'
+from nltk.parse.stanford import StanfordParser
+from nltk.treetransforms import chomsky_normal_form
+from nltk.tree import Tree
+from vector.wordvectors import WordVectors
+parser = StanfordParser(path_to_jar="/Users/HyNguyen/Downloads/stanford-parser-full-2015-12-09/stanford-parser.jar",
+                        path_to_models_jar="/Users/HyNguyen/Downloads/stanford-parser-full-2015-12-09/stanford-parser-3.6.0-models.jar"
+                        ,model_path="/Users/HyNguyen/Downloads/stanford-parser-full-2015-12-09/stanford-parser-3.6.0-models/edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz")
 
-a = [1,2,3,4]
 
-print a[2,1,4]
+if __name__ =="__main__":
+
+    with open("../data/rt-polarity.neg.txt",mode="r") as f:
+        neg_sent = f.readlines()
+
+    with open("../data/rt-polarity.pos.txt",mode="r") as f:
+        pos_sent = f.readlines()
+
+    sents_label = [0]*len(neg_sent) + [1]*len(pos_sent)
+
+    trees = []
+    labels = []
+
+    count = 0
+    for sent, label in zip(neg_sent+pos_sent, sents_label):
+        try:
+            count +=1
+            if count % 200 ==0 :
+                print(count)
+            a = list(parser.raw_parse(sent))
+            hytree = a[0]
+            chomsky_normal_form(hytree)
+            trees.append(hytree[0])
+            labels.append(label)
+        except:
+            continue
+    import pickle
+    with open("hynguyen.pickle",mode="wb") as f:
+        pickle.dump((trees,labels),f)
+
+
